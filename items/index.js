ac8b41cafc56be10acf31272f79763f46f573d67
Emily <snow3341@outlook.com>
2020-12-06 18:22:02 -0500

    change copyright

--- items/index.js
+++ items/index.js
@@ -1,4 +1,4 @@
-export default [{"id": "item1", "fname":"item1.md", "specialImage":"/img/course-registration.jpg", "altText": "registration"},
+export default [{"id": "item1", "fname":"item1.md", "specialImage":"/img/course-registration.jpg", "altText": "registration picture"},
 {"id": "item2", "fname":"item2.md", "specialImage":"/img/hands.jpeg", "altText": "arms"},
 {"id": "item3", "fname":"item3.md", "specialImage":"/img/painting.png", "altText": "artwork"},
 {"id": "item4", "fname":"item4.md", "specialImage":"/img/mural.png", "altText": "wall painting"},
